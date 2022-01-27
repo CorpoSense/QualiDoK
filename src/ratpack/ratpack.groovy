d0b7539559209959c889ec5c296e99058558b120
Ibrahim.H <bitsnaps@yahoo.fr>
2022-01-27 17:32:55 +0100

    add partials

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -3,12 +3,18 @@ import static ratpack.groovy.Groovy.ratpack
 import static ratpack.thymeleaf3.Template.thymeleafTemplate as view
 
 ratpack {
+    serverConfig {
+        port(3000)
+    }
     bindings {
         module (ThymeleafModule)
     }
     handlers {
         get{
-            render(view("index"))
+            Date d = new java.util.Date()
+            render(view("index", [user:'admin']))
         }
+        // Serve assets from 'public'
+        files { dir "public" }
     }
 }
