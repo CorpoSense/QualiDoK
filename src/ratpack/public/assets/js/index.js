9e80602ca983238b3b9078f7e890764d1f47b235
imenebaraitame <baraitame.imene@gmail.com>
2023-05-02 22:07:25 +0100

    Update index.html to display directories using Bootstrap table

--- src/ratpack/public/assets/js/index.js
+++ src/ratpack/public/assets/js/index.js
@@ -1,3 +1,30 @@
-$(document).ready(function () {
-    $('#listDirectories').DataTable();
+var directories = $('#directories').val();
+    var dir = JSON.parse(directories);
+    var columns = [{
+      field: 'id',
+      title: 'ID'
+    }, {
+      field: 'parentId',
+      title: 'parent ID'
+    }, {
+      field: 'name',
+      title: 'Name'
+    }, {
+      field: 'lastModified',
+      title: 'Last Modified'
+    }, {
+      field: 'creator',
+      title: 'Creator'
+    }];
+$(document).ready(function() {
+    $('#table').bootstrapTable({
+       toggle: 'table',
+       search: true,//enables a search box above the table
+       searchable: true,//enables searching for all columns
+       pagination: true,
+       sidePagination: 'client',//all the data will be loaded on the client-side
+       columns: columns ,
+       data:dir
+    });
 });
+
