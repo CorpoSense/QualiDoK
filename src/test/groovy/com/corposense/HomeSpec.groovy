b4eacf49a7215d4033fc2d4f80c5a69245c472bf
Ibrahim.H <bitsnaps@yahoo.fr>
2023-07-06 18:59:26 +0100

    test: 01 hit endpoint

--- src/test/groovy/com/corposense/HomeSpec.groovy
+++ src/test/groovy/com/corposense/HomeSpec.groovy
@@ -7,26 +7,21 @@ import spock.lang.Unroll
 
 class HomeSpec extends Specification {
 
-//    @AutoCleanup
-//    @Shared
-//    GroovyRatpackMainApplicationUnderTest app = new GroovyRatpackMainApplicationUnderTest()
+    @AutoCleanup
+    @Shared
+    GroovyRatpackMainApplicationUnderTest app = new GroovyRatpackMainApplicationUnderTest()
 
     @Unroll
     def 'Response should return ok'() {
-        given:
-            int i = 1
-        expect:
-            i == 1
+        when:
+        def response = aut.httpClient.get()
 
-//        when:
-//        def response = aut.httpClient.get()
-//
-//        then:
-//        response.statusCode == 200
-//
-//        where:
-//            aut | type
-//            app | 'ratpack.groovy'
+        then:
+        response.statusCode == 200
+
+        where:
+            aut | type
+            app | 'ratpack.groovy'
     }
 
 }
